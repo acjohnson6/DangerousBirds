edcdb77715ebaa14e6f1864c3995660924192c03
Adrian Johnson <adrianjohnson@adrianscdesktop.na.ad.espeed.com>
2014-02-07 14:41:00 -0800

    Initial Commit

---
+++ DangerousBirds/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DangerousBirds
+//
+//  Created by Adrian Johnson on 2/7/14.
+//  Copyright (c) 2014 Adrian Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DangerousBirds/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  DangerousBirds
+//
+
+//  Copyright (c) 2014 Adrian Johnson. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
